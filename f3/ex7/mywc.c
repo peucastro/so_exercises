a2c9d146c51fd69f12085f4baab7b4df666db816
Pedro Castro <falecompedroac@gmail.com>
2024-10-22 11:16:35 +0100

    f3:ex7 corrected input flags

--- f3/ex7/mywc.c
+++ f3/ex7/mywc.c
@@ -1,43 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
-    if (argc != 3 || argv[1][0] != '-' || !(argv[1][1] == 'c' || argv[1][1] == 'w' || argv[1][1] == 'l'))
+    int cflag = 0;
+    int wflag = 0;
+    int lflag = 0;
+
+    int c;
+    while ((c = getopt(argc, argv, "cwl")) != -1)
     {
-        printf("Correct usage: ./mywc -flag filename\n\nFlag options:\n    -c : prints the number of characters in the file\n    -w : prints the number of words in the file\n    -l : prints the number of lines in the file\n");
+        switch (c)
+        {
+        case 'c':
+            cflag = 1;
+            break;
+        case 'w':
+            wflag = 1;
+            break;
+        case 'l':
+            lflag = 1;
+            break;
+        default:
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    char *filename = argv[argc - 1];
+    if (filename == NULL)
+    {
+        printf("usage: %s [-c|-w|-l] file\n\nFlag options:\n    -c : prints the number of characters in the file\n    -w : prints the number of words in the file\n    -l : prints the number of lines in the file\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    FILE *file = fopen(argv[2], "r");
+    FILE *file = fopen(filename, "r");
     if (file == NULL)
     {
         printf("error: could not open %s\n", argv[1]);
         exit(EXIT_FAILURE);
     }
 
-    int count = 0;
+    int count;
 
-    if (argv[1][1] == 'c')
+    if (cflag)
     {
+        count = 0;
         for (char c = getc(file); c != EOF; c = getc(file))
             count++;
+        printf("Characters count: %d\n", count);
     }
-    else if (argv[1][1] == 'w')
+    if (wflag)
     {
+        rewind(file);
+        count = 0;
         for (char c = getc(file); c != EOF; c = getc(file))
-            if (c == ' ')
+            if (c == ' ' || c == '\n' || c == '\t')
                 count++;
-        count++;
+        printf("Words count: %d\n", count);
     }
-    else if (argv[1][1] == 'l')
+    if (lflag)
     {
+        rewind(file);
+        count = 0;
         for (char c = getc(file); c != EOF; c = getc(file))
             if (c == '\n')
                 count++;
+        printf("Lines count: %d\n", count);
     }
 
-    printf("%d\n", count);
     fclose(file);
     exit(EXIT_SUCCESS);
 }
