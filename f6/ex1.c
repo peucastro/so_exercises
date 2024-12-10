f0ec3939bc1caa97703401010007ffba77dafc0e
Pedro Castro <falecompedroac@gmail.com>
2024-12-10 11:37:20 +0000

    f6/ex1:done

---
+++ f6/ex1.c
@@ -0,0 +1,49 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 4096
+
+int main(int argc, char *argv[]) {
+  int fd[2];
+  pid_t pid;
+  if (pipe(fd) == -1) {
+    perror("pipe");
+    exit(EXIT_FAILURE);
+  }
+  if ((pid = fork()) == -1) {
+    perror("fork");
+    exit(EXIT_FAILURE);
+  }
+  if (pid > 0) { /* parent */
+    int file = open(argv[1], O_RDONLY);
+    if (file == -1) {
+      perror("error: cannot open file");
+      exit(EXIT_FAILURE);
+    }
+    close(fd[0]);
+    char buffer[BUFFER_SIZE];
+    int nbytes = read(file, buffer, BUFFER_SIZE);
+    while (nbytes > 0) {
+      write(fd[1], buffer, nbytes);
+      nbytes = read(file, buffer, BUFFER_SIZE);
+    }
+    close(file);
+    close(fd[1]);
+    if (wait(NULL) == -1) {
+      perror("wait");
+      exit(EXIT_FAILURE);
+    }
+    exit(EXIT_SUCCESS);
+  } else { /* child */
+    char buffer[BUFFER_SIZE];
+    close(fd[1]);
+    int nbytes = read(fd[0], buffer, BUFFER_SIZE);
+    write(STDOUT_FILENO, buffer, nbytes);
+    close(fd[0]);
+    exit(EXIT_SUCCESS);
+  }
+}
